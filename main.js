fb8e2cc602cad670fc963d8b00fce6aa1800a396
Enes Osmanoğlu <enesosmanoglu@gmail.com>
2021-01-08 13:47:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
+
+Array.prototype.unique = function () {
+    return this.filter((v, i, a) => a.indexOf(v) == i)
+}
+
+
 String.prototype.escapeUnicode = function () {
     return this.replace(/\\\\u([0-9a-f]{4})/g, (whole, group1) => String.fromCharCode(parseInt(group1, 16)))
 }
 String.prototype.getBetweenAll = function () {
     let queries = Array.from(arguments)
-    let result = text.split(queries.shift()).slice(1).map(i => { if (!queries.length) return i.escapeUnicode(); return (queries.length > 1 ? queries.reduce((a, b) => i.split(a)[0].split(b)[0]) : i.split(queries[0])[0]).escapeUnicode() })
+    queries = queries.map(s=>s.replace(/\[/g,"\\["))
+    let result = Array.from(this.match(new RegExp(`(?<=${queries[0]})(.*?)(?=\s*${queries[1]})`, "g")))
     return result
-    /* 
-    if (groupEveryX) {
-        let groups = []
-        for (let i = 0; i < result.length; i++) {
-            let group = []
-            for (let j = 0; j < groupEveryX; j++) {
-                group.push((result[i++]).escapeUnicode())
-            }
-            groups.push(group)
-            i--;
-        }
-        return groups
-    } else {
-        return result
-    } */
 }
 String.prototype.getBetween = function () {
-    return this.getBetweenAll(arguments)
+    return this.getBetweenAll(...arguments)[0]
 }
 
 /**
@@ -64,10 +56,6 @@ Object.prototype.find = function (key) {
     }
 
     return undefined;
-}
-
-Array.prototype.unique = function () {
-    return this.filter((v, i, a) => a.indexOf(v) == i)
 }
 
 Array.prototype.clean = function () {
