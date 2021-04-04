0d71971cef912098abbcb2ddd24cf8d6438fc96b
Enes Osmanoğlu <enesosmanoglu@gmail.com>
2021-04-05 01:05:20 +0300

    arrangement

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-
-Array.prototype.unique = function () {
-    return this.filter((v, i, a) => a.indexOf(v) == i)
-}
-
-
+// STRING
 String.prototype.escapeUnicode = function () {
     return this.replace(/\\\\u([0-9a-f]{4})/g, (whole, group1) => String.fromCharCode(parseInt(group1, 16)))
 }
 String.prototype.getBetweenAll = function () {
     let queries = Array.from(arguments)
-    queries = queries.map(s=>s.replace(/\[/g,"\\["))
+    queries = queries.map(s => s.replace(/\[/g, "\\["))
     let result = Array.from(this.match(new RegExp(`(?<=${queries[0]})(.*?)(?=\s*${queries[1]})`, "g")))
     return result
 }
@@ -17,12 +12,7 @@ String.prototype.getBetween = function () {
     return this.getBetweenAll(...arguments)[0]
 }
 
-/**
- * Same as Array.prototype.find but for all objects :)
- * @param {(string|Function)} predicate key || Function( key:string, value:any, object:Object)
- * @returns First object which contains provided key.
- * @returns First object which matches with provided function.
- */
+// OBJECT
 Object.prototype._find = function (predicate) {
     for (var i = 0; i < Object.keys(this).length; i++) {
         try {
@@ -57,18 +47,21 @@ Object.prototype.find = function (key) {
 
     return undefined;
 }
-
-Array.prototype.clean = function () {
-    return this.filter(a => a != undefined && a != null)
-}
-
-Object.prototype.map = function(cb) {
+Object.prototype.map = function (cb) {
     return Object.fromEntries(Object.keys(this).map((v, i, a) => [v, cb(this[v], v, this)]));
 }
 Object.prototype.mapValue = Object.prototype.map;
-Object.prototype.mapKey = function(cb) {
+Object.prototype.mapKey = function (cb) {
     return Object.fromEntries(Object.keys(this).map((v, i, a) => [cb(this[v], v, this), this[v]]));
 }
-Object.prototype.mapKeyValue = function(cb) {
+Object.prototype.mapKeyValue = function (cb) {
     return Object.fromEntries(Object.keys(this).map((v, i, a) => cb(this[v], v, this)));
+}
+
+// ARRAY
+Array.prototype.unique = function () {
+    return this.filter((v, i, a) => a.indexOf(v) == i)
+}
+Array.prototype.clean = function () {
+    return this.filter(a => a != undefined && a != null)
 }
