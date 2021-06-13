aa0551a0b7bda19f4004e49dd5960b5de0f4c8b0
Enes Osmanoğlu <enesosmanoglu@gmail.com>
2021-06-13 03:29:03 +0300

    update

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 // STRING
 String.prototype.escapeUnicode = function () {
     return this.replace(/\\\\u([0-9a-f]{4})/g, (whole, group1) => String.fromCharCode(parseInt(group1, 16)));
-}
+};
+String.prototype.escapeRegExp = function () {
+    return this.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+};
 String.prototype.getBetweenAll = function () {
     let queries = Array.from(arguments);
     queries = queries.map(s => s.replace(/\[/g, "\\["));
     let result = Array.from(this.match(new RegExp(`(?<=${queries[0]})(.*?)(?=\s*${queries[1]})`, "g")));
     return result;
-}
+};
 String.prototype.getBetween = function () {
     return this.getBetweenAll(...arguments)[0];
-}
+};
 String.prototype.padCenter = function (maxLength, fillString = " ") {
     return this.padStart(this.length + Math.floor((maxLength - this.length) / 2), fillString).padEnd(maxLength, fillString);
-}
+};
+String.prototype.replaceAll = function (searchValue, replaceValue) {
+    return this.replace(new RegExp(searchValue, "g"), replaceValue);
+};
+String.prototype.replaceAllWithArray = function (searchValue, replaceArray = []) {
+    let str = this;
+    let match = this.match(new RegExp(searchValue.escapeRegExp()));
+    if (!match || !replaceArray.length) return str.toString();
+    str = str.replace(searchValue, replaceArray.shift());
+    return str.replaceAllWithArray(searchValue, replaceArray).toString();
+};
+String.prototype.format = function () {
+    let str = this.toString();
+    if (arguments.length) {
+        let t = typeof arguments[0];
+        let key;
+        let args = ("string" === t || "number" === t) ? Array.prototype.slice.call(arguments) : arguments[0];
+        for (key in args) {
+            str = str.replace(new RegExp("\\{" + key + "\\}", "gi"), args[key]);
+        }
+    }
+    return str;
+};
 
 // OBJECT
 Object.prototype._find = function (predicate) {
@@ -31,50 +56,46 @@ Object.prototype._find = function (predicate) {
     }
 
     return undefined;
-}
+};
 Object.prototype.findAll = function (key) {
     try {
         return JSON.stringify(this).split(`"${key}"`).slice(1).map(t => t.split("}")[0].split(`,"`)[0].replace(":", "")).map(v => eval(v));
     } catch (err) { }
 
     return undefined;
-}
+};
 Object.prototype.find = function (key) {
     try {
         return this.findAll(key)[0]
     } catch (err) { }
 
     return undefined;
-}
+};
 Object.prototype.map = function (cb) {
     return Object.fromEntries(Object.keys(this).map((v, i, a) => [v, cb(this[v], v, this)]));
-}
+};
 Object.prototype.mapValue = Object.prototype.map;
 Object.prototype.mapKey = function (cb) {
     return Object.fromEntries(Object.keys(this).map((v, i, a) => [cb(this[v], v, this), this[v]]));
-}
+};
 Object.prototype.mapKeyValue = function (cb) {
     return Object.fromEntries(Object.keys(this).map((v, i, a) => cb(this[v], v, this)));
-}
+};
 
 // ARRAY
 Array.prototype.unique = function () {
     return this.filter((v, i, a) => a.indexOf(v) == i);
-}
-Array.prototype.clear = function () {
-    this = this.filter(a => a != undefined && a != null);
-    return this;
-}
+};
 Array.prototype.clean = function () {
     return this.filter(a => a != undefined && a != null);
-}
+};
 Array.prototype.shuffle = function () {
     for (let i = this.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [this[i], this[j]] = [this[j], this[i]];
     }
     return this;
-}
+};
 Array.prototype.shuffled = function () {
     let array = Array.from(this);
     for (let i = array.length - 1; i > 0; i--) {
@@ -82,4 +103,4 @@ Array.prototype.shuffled = function () {
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-}
+};
